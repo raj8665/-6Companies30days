dc6f456be1799387d083a0eb37ef06e0f8f989e2
Raj Chaturvedi <rajchaturvedi07032004@gmail.com>
2025-01-16 22:25:55 +0530

    Day-2 (Amazon)

---
+++ Day-2(Amazon)/Excel Sheet Column Title.cpp
@@ -0,0 +1,14 @@
+// Leetcode - 168 
+
+string convertToTitle(int n) {
+        string ans;
+        while(n > 0) {
+            n--;
+            int curr = n % 26;
+            n = n / 26;
+            ans.push_back(curr + 'A');
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+        
+    }
