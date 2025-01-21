41b3d8ea431d7c15a56ba7e7d5055fd0db4f0dfc
Raj Chaturvedi <rajchaturvedi07032004@gmail.com>
2025-01-21 17:10:01 +0530

    Day-5 (Amazon)

--- Day-5 (Amazon)/Brackets in Matrix Chain Multiplication.cpp
+++ Day-5 (Amazon)/Brackets in Matrix Chain Multiplication.cpp
@@ -2,36 +2,44 @@
 
 // Just MCM + Memoize Dp
 
-int solve(int i, int j, vector<vector<int>>& dp, 
-    vector<vector<string>>& solution, vector<int>& arr) {
-    if (i == j) {
-        solution[i][j] = string(1, 'A' + i - 1);
-        return 0;
-    }
-    if (dp[i][j] != -1) return dp[i][j];
-
-    int mini = INT_MAX;
-    string bestOrder;
-
-    for (int k = i; k < j; k++) {
-        int cost = solve(i, k, dp, solution, arr) + solve(k + 1, j, dp, solution, arr) + 
-        arr[i - 1] * arr[k] * arr[j];
-        if (cost < mini) {
-            mini = cost;
-            bestOrder = "(" + solution[i][k] + solution[k + 1][j] + ")";
+// question mein bs upper triangular matrix use ho rha h isliye answer s[1][n-1]
+    // pe mil rha h if there is confustion then ask chatgpt to fill dp table and s table 
+    // for any one of the example 
+    int solve(int i, int j, vector<vector<int>> &dp, vector<int> &arr, 
+    vector<vector<string>> &s){
+        // Two Base Cases
+        if(i>j) return 0;
+        if(i==j) {
+            s[i][j] = string(1, 'A' + i - 1);
+            return 0;
         }
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        string ans;
+        int mini = INT_MAX;
+        
+        for(int k=i;k<j;k++){
+            // Don't confuse in the index of arr and s they both have different function
+            int cost = solve(i,k,dp,arr,s) + solve(k+1,j,dp,arr,s) +
+            arr[i-1]*arr[k]*arr[j];
+            
+            if(cost<mini){
+                mini = cost;
+                ans = "(" + s[i][k] + s[k+1][j] + ")";
+            }
+        }
+        
+        s[i][j] = ans;
+        return dp[i][j] = mini;
+        
     }
-
-    solution[i][j] = bestOrder;
-    return dp[i][j] = mini;
-   }
-
     string matrixChainOrder(vector<int> &arr) {
         // code here
-    int N = arr.size();
-    vector<vector<int>> dp(N, vector<int>(N, -1));
-    vector<vector<string>> solution(N, vector<string>(N, ""));
-
-    solve(1, N - 1, dp, solution, arr);
-    return solution[1][N - 1];
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int> (n,-1));
+        vector<vector<string>> s(n, vector<string> (n,""));
+        solve(1,n-1,dp,arr,s);
+        // kuki hm sirf upper triangular matrix hi use kr rhe h 
+        return s[1][n-1];
     }
