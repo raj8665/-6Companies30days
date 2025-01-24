d6f4e562cb42f55e91c1115110049125393b5391
Raj Chaturvedi <rajchaturvedi07032004@gmail.com>
2025-01-24 23:33:12 +0530

    Day-7 (Miicrosoft)

--- Day-7 (Microsoft)/Image Smoother.cpp
+++ Day-7 (Microsoft)/Image Smoother.cpp
@@ -31,4 +31,74 @@
         return ans;
     }
 
-    
+// Solution 2 - Takes O(m) space just store prevRow and prevCorner and update the give matrix 
+
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        // Just take any one of the cell and think of what we required just prevRow and one of prev
+        // corner of the same row 
+        // Now we take extra space O(n) for storing just prev row 
+         // Save the dimensions of the image.
+        int n = img.size();
+        int m = img[0].size();
+
+        // Create temp array of size n.
+        vector<int> prevRow(m);
+        // ye prev Corner isliye chahiye kuki jb hm prevRow ko update krenge to ye update ho
+        // to isko hm store kr lenge 
+        int prevCorner = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int curSum = 0;
+                int cnt = 0;
+
+                // for checking upper row
+                if(i-1>=0){
+                    curSum+= prevRow[j];
+                    cnt++;
+                    if(j-1>=0){
+                        curSum+= prevCorner;
+                        cnt++;
+                    }
+                    if(j+1<m){
+                        curSum+= prevRow[j+1];
+                        cnt++;
+                    }
+                }
+                // for checking the same row
+                if(j-1>=0){
+                    curSum+= prevRow[j-1];
+                    cnt++;
+                }
+                if(j+1<m){
+                    curSum += img[i][j+1];
+                    cnt++;
+                }
+                curSum += img[i][j];
+                cnt++;
+                // for checking down row
+                if(i+1<n){
+                    curSum += img[i+1][j];
+                    cnt++;
+                    if(j-1>=0){
+                        curSum+=img[i+1][j-1];
+                        cnt++;
+                    }
+                    if(j+1<m){
+                        curSum += img[i+1][j+1];
+                        cnt++;
+                    }
+                }
+              
+              if(i-1>=0){
+                prevCorner = prevRow[j];
+              }
+              prevRow[j] = img[i][j];
+
+              img[i][j] = curSum/cnt;
+            }
+        }
+
+        // Return the smooth image.
+        return img;
+    }
--- Day-7 (Microsoft)/Minimum Moves to Equal Array Elements II.cpp
+++ Day-7 (Microsoft)/Minimum Moves to Equal Array Elements II.cpp
@@ -1,2 +1,33 @@
  
     // Leetcode - 462
+
+// Solution -1 : O(nlogn + n) solution nlogn for sorting but this is basic approach not satisfied with this extra nlogn for sorting 
+
+int minMoves2(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+
+        int median = nums[n/2];
+
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            cnt += abs(median-nums[i]);
+        }
+        return cnt;
+    }
+
+// Solution - 2 : Trying to remove this extra O(nlogn) for sorting and solve it in O(n) time complexity 
+ int minMoves2(vector<int>& nums) {
+        int n = nums.size();
+
+        // Quick Sort C++ STL as quick Sort take O(n) t.c.
+        nth_element(nums.begin(), nums.begin()+(n/2), nums.end());
+
+        int median = nums[n/2];
+
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            cnt += abs(median-nums[i]);
+        }
+        return cnt;
+    }
